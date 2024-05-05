532128d880de700ccc6d8041b3401d7a36f5aaec
SharonAliyas5573 <sharonaliyas25@gmail.com>
2024-05-05 13:29:56 +0530

    Set up swagger

--- TO_DO_DRF/urls.py
+++ TO_DO_DRF/urls.py
@@ -15,12 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
-
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDo-API Documentation",
+      default_version='v1',
+      description="API documentation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('auth/', include('auth.urls')),
     path('task/', include('task.urls')),
 ]
