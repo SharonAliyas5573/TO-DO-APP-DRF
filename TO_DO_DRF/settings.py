95436d4f18b04c191a89fe424d50580dc9a6b648
SharonAliyas5573 <sharonaliyas25@gmail.com>
2024-05-06 20:41:27 +0530

    Add REST framework configuration

--- TO_DO_DRF/settings.py
+++ TO_DO_DRF/settings.py
@@ -54,6 +54,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 ROOT_URLCONF = 'TO_DO_DRF.urls'
 
 TEMPLATES = [
